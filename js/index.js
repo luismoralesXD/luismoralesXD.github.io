c1fbd29e48c70d424e85cc2fca589a312f71bc9b
LuisMoraleles <luis._.26@icloud.com>
2017-09-05 20:15:40 -0700

    web project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,63 @@
-$("#ShowAboutButton").click(function(){
-    $("#About").slideToggle();
+$("#Title1").click(function(){
+    $("#About1").slideToggle();
 });
 
-$("#ShowAboutButton2").click(function(){
+$("#Title2").click(function(){
     $("#About2").slideToggle();
+});
+
+$("#Title3").click(function(){
+    $("#About3").slideToggle();
+});
+
+$("#Title4").click(function(){
+    $("#About4").slideToggle();
+});
+
+$("#Title5").click(function(){
+    $("#About5").slideToggle();
+});
+
+$("#Title6").click(function(){
+    $("#About6").slideToggle();
+});
+
+$("#Title7").click(function(){
+    $("#About7").slideToggle();
+});
+
+$("#Title8").click(function(){
+    $("#About8").slideToggle();
+});
+
+$("#Title9").click(function(){
+    $("#About9").slideToggle();
+});
+
+$("#Title10").click(function(){
+    $("#About10").slideToggle();
+});
+
+$("#Title11").click(function(){
+    $("#About11").slideToggle();
+});
+
+$("#Title12").click(function(){
+    $("#About12").slideToggle();
+});
+
+$("#Title13").click(function(){
+    $("#About13").slideToggle();
+});
+
+$("#Title14").click(function(){
+    $("#About14").slideToggle();
+});
+
+$("#Title15").click(function(){
+    $("#About15").slideToggle();
+});
+
+$("#Title16").click(function(){
+    $("#About16").slideToggle();
 });
